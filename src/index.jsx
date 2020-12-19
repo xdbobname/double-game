a5f49106abcb879bcc4fc657fbbba4d77d542c62
xdbobname <1908496390@qq.com>
2020-12-19 11:09:44 +0800

    continus move & share realtime screen

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,26 @@
 class GamePaint extends React.Component{
     constructor(props) {
         super(props);
+        if (props.onMove2Next) {
+            this.onMove2Next = props.onMove2Next;
+        }
+        if (props.onCatchGift) {
+            this.onCatchGift = props.onCatchGift;
+        }
         this.paintData = props.paintData;
         this.state={
             data: [{x:40,y:60},{x:40,y:80},{x:60,y:80}],
-            goal: [],
+            score:0,
             nextDirection: { x: 0, y: 1 },
         }
         this.tempDirection = { x: 0, y: 1 }
         this.running = false;
         this.paintBoard = React.createRef()
         this.drawRec = this.drawRec.bind(this);
-        this.drawBackground = this.drawBackground;
-        this.toStart = this.toStart.bind(this)
+        this.drawBackground = this.drawBackground.bind(this);
+        this.toStart = this.toStart.bind(this);
+        this.drawGift = this.drawGift.bind(this);
+        this.geneGift = this.geneGift.bind(this);
 
     }
     componentDidMount() {
@@ -40,7 +48,30 @@ class GamePaint extends React.Component{
         board.stroke();
         ///this.background=canvas.toDataURL("image/webp")
     }
-
+    geneGift() {
+        const canvas = this.paintBoard.current;
+        const board = canvas.getContext('2d');
+        let baseheight =canvas.height/20|0;
+        let basewidth = canvas.width / 20 | 0;
+        let x, y;
+        do {
+            x = 20 * (Math.random() * basewidth | 0);
+            y = 20 * (Math.random() * baseheight| 0);
+        }
+        while (this.state.data.includes({
+            x,y
+        }))
+        //console.log(x,y)
+        return {
+            x,y
+        }
+    }
+    drawGift(pos) {
+        const canvas = this.paintBoard.current;
+        const board = canvas.getContext('2d');
+        board.fillStyle = "blue";
+        board.fillRect(pos.x,pos.y, 20, 20);
+    }
     getDirection() {
         const data = this.state.data;
         const de1 = data[0];
@@ -49,12 +80,29 @@ class GamePaint extends React.Component{
         return {
             direction: this.state.nextDirection,
             tailDirection: {
-                x: (de2.x - de1.x)/20,
-                y:(de2.y-de1.y)/20
+                x: Math.sign(de2.x - de1.x),
+                y: Math.sign(de2.y-de1.y)
             }
         }
     }
-    drawData(data,dis,dir) {
+    drawData(data, dis, dir) {
+        if (dis == 2) {
+            
+            let head = data.slice(-1)[0];
+            if (Math.abs(head.x - this.gift.x) + Math.abs(head.y - this.gift.y) < 20) {
+                //console.log(head,data,dis,dir);
+                data.push(
+                    this.gift
+                )
+                this.setState({
+                    data
+                })
+                if (this.onCatchGift) {
+                    this.gift = this.geneGift();
+                    this.onCatchGift(this.state, this.gift);
+                }
+            }
+        }
         const board = this.paintBoard.current.getContext('2d');
         board.lineWidth = 2;
         board.fillStyle = "lime"
@@ -71,10 +119,15 @@ class GamePaint extends React.Component{
         board.fillRect(dx.x+dir.direction.x*dis, dx.y+dir.direction.y*dis, 20, 20);
     }
     
+
     toStart(curDir) {
+        if (!this.gift) {
+            this.gift = this.geneGift();
+            //console.log("fail")
+        }
+        if(curDir&&(curDir.x+this.state.nextDirection.x||curDir.y+this.state.nextDirection.y))
         this.tempDirection = curDir;
         if (this.running) {
-            
             return;
         }
         this.running = true;
@@ -86,6 +139,7 @@ class GamePaint extends React.Component{
             //console.log(1);
             const cb = () => {
                 this.drawBackground();
+                this.drawGift(this.gift);
                 this.drawData(this.state.data, dis,dir)
                 dis++;
                 if (dis <= 20) {
@@ -95,14 +149,21 @@ class GamePaint extends React.Component{
                 this.setState({
                     nextDirection: this.tempDirection,
                     data: this.state.data.slice(1).concat({x:dx.x+dir.direction.x*20,y:dx.y+dir.direction.y*20})
+                }, function(){
+                    if (this.onMove2Next) {
+                    this.onMove2Next(this.state, this.gift);
+                }
                 })
-                this.running = false;
-                this.toStart(this.tempDirection)
+                setTimeout(() => {
+                    this.running = false;
+                    this.toStart(this.tempDirection)
+                },17)
+                
             }
         requestAnimationFrame(cb)
     }
     drawRec() {
-        console.log(this.paintBoard.current);
+        //console.log(this.paintBoard.current);
         const board = this.paintBoard.current.getContext('2d');
         board.lineWidth = 2;
         var x1 = 100, y1 = 50,x2=33,y2=33;
@@ -129,8 +190,8 @@ class GamePaint extends React.Component{
         return (<div className="gameArea">
             <canvas width="600px" height="400px" className="paintBox" ref={this.paintBoard}></canvas>
             <br/>
-            <button onClick={this.drawRec}>helo</button>
-            <button onClick={this.toStart}>start</button>
+            <button onClick={this.drawRec}>画矩形测试</button>
+            <label>{this.state.score} 分</label>
         </div>)
     }
 }
